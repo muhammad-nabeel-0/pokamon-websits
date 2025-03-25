092ffa5d6429b62955b074b096d495d938bef9f5
M Nabeel <nabeelgaming786yt@gmail.com>
2025-03-25 16:06:23 +0500

    slove card promble

--- src/components/SkeletonLoader.jsx
+++ src/components/SkeletonLoader.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import './SkeletonLoader.css'
 
 const SkeletonLoader = () => {
     return (
         <div className="mian-div">
-
         <div className="skeleton-container">
             {Array.from({ length: 12 }).map((_, index) => (
                 <div key={index} className="skeleton-card">
